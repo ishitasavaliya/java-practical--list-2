360e5be509973c8f630c0ca1b2af60c7bb12ff32
ishitasavaliya <141223316+ishitasavaliya@users.noreply.github.com>
2024-02-20 14:19:50 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Add{
+		Add(int a,int b)
+		{
+			System.out.print("addition is::"+(a+b));
+		}
+	}
+public class P1{
+	public static void main(String s[])
+	{
+		
+		Add a1=new Add(40,40);
+	}
+}
+	
